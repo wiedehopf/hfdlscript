b6ca5208eb0ac8972a612335a0cc3475af63abc5
Matthias Wirth <matthias.wirth@gmail.com>
2022-03-17 21:22:38 +0100

    use highest position count for frequency selection if possible

--- hfdl.sh
+++ hfdl.sh
@@ -89,20 +89,25 @@ fi
 
 i=0
 count=()
+positions=()
 for x in "${freq[@]}"
 do
     count+=(0)
+    positions+=(0)
     if [ -f "/home/pi/hflog/hf.log" ]; then
         rm /home/pi/hflog/hf.log
     fi
     timeout "$TIMEOUT" $dumpcmd --gain-elements ${gain[$i]} --sample-rate ${samp[$i]} ${freq[$i]} >/dev/null
     if [ -f "/home/pi/hflog/hf.log" ]; then
-        count[$i]=`grep -c "Lat" /home/pi/hflog/hf.log`
+        count[$i]=`grep -c "kHz" /home/pi/hflog/hf.log`
+        positions[$i]=`grep -c "Lat:" /home/pi/hflog/hf.log`
     fi
+    echo ${fname[$i]} messageCount: ${count[$i]} positionCount: ${positions[$i]}
     (( i += 1 ))
 done
 
 j=0
+k=0
 
 i=0
 for x in "${freq[@]}"
@@ -111,17 +116,30 @@ do
     then
         j=$i
     fi
+    if (( ${positions[$i]} > ${positions[$j]} ))
+    then
+        k=$i
+    fi
     (( i += 1 ))
 done
+echo Highest message count: ${fname[$j]} received "${count[$j]}" messages.
+echo Highest position count: ${fname[$k]} received "${positions[$k]}" positions.
 
 #end of testing for active frequencies
 
+if [ $k -gt 0 ]
+then
+    x=$k
+else
+    x=$j
+fi
+
 #Display the friendly name, gain elements, sample rate and active frequencies chosen by the script when running it manually in a terminal
-echo "Using ${fname[$j]}: gain-elements ${gain[$j]}, sample-rate ${samp[$j]}, frequencies ${freq[$j]}"
+echo "Using ${fname[$x]}: gain-elements ${gain[$x]}, sample-rate ${samp[$x]}, frequencies ${freq[$x]}"
 
 #this ends the script and runs dumphfdl using the above parameters and the most-acive frequency array using its gain reduction settings and sampling rate
 
 #NOTE: if something is wrong with your script it will always default to using the first frequency array
 
-$dumpcmd --gain-elements ${gain[$j]} --sample-rate ${samp[$j]} ${freq[$j]} & >/dev/null
+$dumpcmd --gain-elements ${gain[$x]} --sample-rate ${samp[$x]} ${freq[$x]} & >/dev/null
 
