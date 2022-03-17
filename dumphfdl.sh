7068e58d3856dab14b8e5ef139ed31228649af12
Matthias Wirth <matthias.wirth@gmail.com>
2022-03-17 20:07:31 +0100

    remove necessity of manually handling count array

--- dumphfdl.sh
+++ dumphfdl.sh
@@ -29,9 +29,6 @@ samp=()
 #different gain reduction settings for each frequency group to allow optimizing performance 
 gain=()
 
-#count of frequency group, just keep it like it is for every frequency block
-count=()
-
 #include an optimal sampling rate, gain settings, a count 0 and a friendly name for each frequency group if you have changed them to add more groups
 
 # group 1
@@ -39,35 +36,30 @@ fname+=("5M6M")
 freq+=("5451 5502 5508 5514 5529 5538 5544 5547 5583 5589 5622 5652 5655 5720 6529 6535 6559 6565 6589 6619 6661")
 samp+=("2000000")
 gain+=("IFGR=45,RFGR=4")
-count+=(0)
 
 # group 2
 fname+=("8M10M")
 freq+=("8825 8834 8843 8851 8885 8886 8894 8912 8921 8927 8936 8939 8942 8948 8957 8977 10027 10060 10063 10066 10075 10081 10084 10087 10093")
 samp+=("2000000")
 gain+=("IFGR=53,RFGR=2")
-count+=(0)
 
 # group 3
 fname+=("11M13M")
 freq+=("11306 11312 11318 11321 11327 11348 11354 11384 11387 11184 11306 13264 13270 13276 13303 13312 13315 13321 13324 13342 13351 13354")
 samp+=("3000000")
 gain+=("IFGR=53,RFGR=2")
-count+=(0)
 
 # group 4
 fname+=("17M")
 freq+=("17901 17912 17916 17919 17922 17928 17934 17958 17967")
 samp+=("2000000")
 gain+=("IFGR=40,RFGR=2")
-count+=(0)
 
 # group 5
 fname+=("21M")
 freq+=("21928 21931 21934 21937 21949 21955 21982 21990 21995 21997")
 samp+=("2000000")
 gain+=("IFGR=40,RFGR=2")
-count+=(0)
 
 
 
@@ -91,8 +83,10 @@ dumpcmd="/usr/local/bin/dumphfdl --statsd 192.168.1.156:8125
     --output decoded:text:file:path=/home/pi/hflog/hfdl.log  "
 
 i=0
+count=()
 for x in "${freq[@]}"
 do
+    count+=(0)
     if [ -f "/home/pi/hflog/hf.log" ]; then
         rm /home/pi/hflog/hf.log
     fi
